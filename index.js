b5b7bbe9e825da5dba3982846a4677c620d58915
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2024-09-25 22:19:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello world")
+
+let firstName = 'srikanth'
+let lastName = 'ratnala'
+
+console.log(firstName + ' ' +lastName)
+
+const pi = 3.14
+let radius = 3
+console.log("The circumference of 3 radius circle is: " + (pi*(radius)**2))
