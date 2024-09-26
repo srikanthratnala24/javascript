153aced169afaac6168f984e016e2db5fbecf148
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2024-09-27 00:31:46 +0530

    types of categories

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 console.log("Hello world")
 
-let firstName = 'srikanth'
-let lastName = 'ratnala'
+let firstName = 'srikanth';
+let lastName = 'ratnala';
 
 console.log(firstName + ' ' +lastName)
 
 const pi = 3.14
 let radius = 3
 console.log("The circumference of 3 radius circle is: " + (pi*(radius)**2))
+
+//Primitive types
+
+let name = 'srikanth';
+let age =28;
+let isapproved = false;
+let status = undefined;
+let isonlie = null;
+
+typeof name;
+typeof age;
