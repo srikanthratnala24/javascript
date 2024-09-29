8e79dafbd6a6355b2cdc56a86c107ceb257b1562
srikanthratnala24 <ratnalasrikanth2@gmail.com>
2024-09-29 11:43:13 +0530

    Basics of arrrays objects functions

--- index.js
+++ index.js
@@ -19,3 +19,37 @@ let isonlie = null;
 
 typeof name;
 typeof age;
+
+
+fruits = ['apple','orange','kiwi']
+
+for (i=0; i<fruits.length; i++){
+    console.log(fruits[i])
+}
+
+let htmlContent = '';
+// let out = fruits.forEach(fruit => console.log(fruit));
+fruits.forEach(function(fruit) {
+    // Append each fruit inside an <h1> tag
+    htmlContent += `<h1>${fruit}</h1>`;
+  });
+
+document.getElementById('output').innerHTML = fruits;
+document.getElementById('loop').innerHTML = htmlContent;
+
+document.getElementById('func').innerHTML = greet();
+//functions
+
+function greet(){
+    // console.log("Hello world")
+    return("Hello world");
+}
+
+// calculating  a value
+
+function square(number){
+    return number*number;
+}
+
+let square_num = square(2);
+document.getElementById('square').innerHTML = square_num;
